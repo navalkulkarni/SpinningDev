333fb9bce37362ce7feaebdf6303fcf63a1a2bdd
Naval Kulkarni <navalkulkarni@gmail.com>
2021-10-17 21:12:12 +0530

    add createRecord using lightningUI.

--- force-app/main/default/lwc/enterDailyElectricityUnits/enterDailyElectricityUnits.js
+++ force-app/main/default/lwc/enterDailyElectricityUnits/enterDailyElectricityUnits.js
@@ -1,8 +1,13 @@
 import { api, LightningElement } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import DAILY_ELECTRICITY_UNITS from '@salesforce/schema/Daily_Electricity_Units__c';
+import DATE_TIME from '@salesforce/schema/Daily_Electricity_Units__c.Select_Date_and_Time__c';
+import  UNITS from '@salesforce/schema/Daily_Electricity_Units__c.Units_for_the_Day__c';
 
 export default class EnterDailyElectricityUnits extends LightningElement {
 
-    
+    deuId;
     dateTime;
     units;
 
@@ -18,12 +23,41 @@ export default class EnterDailyElectricityUnits extends LightningElement {
     
     saveRecord(event)
     {
-        console.log('Save Pressed'+ this.units + this.dateTime);
-    }
+        const fields = {};
+        fields[DATE_TIME.fieldApiName] = this.dateTime;
+        fields[UNITS.fieldApiName] = this.units;
+        const recordInput = { apiName: DAILY_ELECTRICITY_UNITS.objectApiName, fields};
+        createRecord(recordInput).then(
+            deu => {
+                this.deuId = deu.id;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Record created',
+                        variant: 'success',
+                    }),
+                );
+            }
+        ).catch(error => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error creating record',
+                    message: error.body.message,
+                    variant: 'error',
+                }),
+            );
+        });
+        
+
+   }
 
     cancelSave(event)
     {
-        console.log('Cancel Pressed');
+       console.log('new log');
+      console.log('cancel pressed  '+ this.template.querySelector('lightning-input[name="units"]').innerText + 'added');
+      //this.template.querySelector('lightning-input[name="units"]').innerHTML;
+       
+       //this.template.querySelector('lightning-input[name="units"]').value='';
     }
 
 }
