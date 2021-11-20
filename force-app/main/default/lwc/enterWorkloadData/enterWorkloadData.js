72d151b8e2116d6cac080a7b1b9395277b724a54
Naval Kulkarni <navalkulkarni@gmail.com>
2021-11-20 19:39:02 +0530

    add worker names to list and display on page

--- force-app/main/default/lwc/enterWorkloadData/enterWorkloadData.js
+++ force-app/main/default/lwc/enterWorkloadData/enterWorkloadData.js
@@ -3,9 +3,19 @@ import { LightningElement } from 'lwc';
 export default class EnterWorkloadData extends LightningElement {
 
     inputValue;
+    listOfNames = [];
 
     handleClick(event){
-        
+        this.inputValue = this.template.querySelector('.workerName').value;
+        console.log('input value is new:'+this.inputValue);
+        this.listOfNames.push(this.inputValue);
+       
+        for (let index = 0; index < this.listOfNames.length; index++) {
+            const element = this.listOfNames[index];
+            console.log('List element is : '+element);
+        }
+        this.template.querySelector('.workerName').value='';
+                
     }
 
 }
