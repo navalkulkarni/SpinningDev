a0c864e9fe71f6a8d2523e5103ef943d2f6bb49e
Naval Kulkarni <navalkulkarni@gmail.com>
2021-11-25 18:53:41 +0530

    add logic to fetch data from org

--- force-app/main/default/lwc/enterWorkloadData/enterWorkloadData.js
+++ force-app/main/default/lwc/enterWorkloadData/enterWorkloadData.js
@@ -1,21 +1,46 @@
 import { LightningElement } from 'lwc';
+import getWorkers from '@salesforce/apex/WorkloadDataController.getWorkers';
 
 export default class EnterWorkloadData extends LightningElement {
 
     inputValue;
     listOfNames = [];
+    orgResult;
+    error;
 
     handleClick(event){
         this.inputValue = this.template.querySelector('.workerName').value;
         console.log('input value is new:'+this.inputValue);
         this.listOfNames.push(this.inputValue);
+        this.template.querySelector('.workerName').value='';
        
         for (let index = 0; index < this.listOfNames.length; index++) {
             const element = this.listOfNames[index];
             console.log('List element is : '+element);
         }
-        this.template.querySelector('.workerName').value='';
+        
+        //console.log('Data from Salesforce :'+this.orgResult);
                 
+    }
+
+    handleClearList(event){
+        this.listOfNames = [];
+        this.template.querySelector('.workerName').value='';
+    }
+
+    handleAbsentClick(event)
+    {
+        getWorkers({workerNames : this.listOfNames})
+        .then((result) => {
+            
+            this.orgResult = result;
+            console.log('Data from Salesforce :'+ result);
+            this.error = undefined;
+        }).catch((error)=>{
+            this.error = error;
+            this.orgResult = undefined;
+        })
+
     }
 
 }
