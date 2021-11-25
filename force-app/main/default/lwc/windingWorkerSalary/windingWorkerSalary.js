bacaff999f8b938fcba01a3ece47f68cdf3cbf6e
Naval Kulkarni <navalkulkarni@gmail.com>
2021-11-25 19:25:02 +0530

    create comp to calculate winding worker salary

---
+++ force-app/main/default/lwc/windingWorkerSalary/windingWorkerSalary.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+import createCountInformation from '@salesforce/apex/WindingWorkerSalaryCalculator.createCountInformation';
+
+
+export default class WindingWorkerSalary extends LightningElement {
+
+    workerId;
+    yarnCount;
+    monthName;
+    slub40Total;
+
+    handleSalaryClick()
+    {
+        
+    }
+
+
+}
